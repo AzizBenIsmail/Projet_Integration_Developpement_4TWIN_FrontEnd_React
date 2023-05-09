d4931c482954d34594a8036840a7f117cbc052d3
oumayma touil <96637712+Oumaymatouil@users.noreply.github.com>
2023-05-09 12:15:03 +0100

    fix profile

--- src/views/Frontend/User/profileDetails.js
+++ src/views/Frontend/User/profileDetails.js
@@ -107,7 +107,7 @@ const ProfileDetails = (props) => {
           <Col className="order-lg-4 text-lg-right align-self-lg-center mr--4" lg="4">
             <div className="card-profile-actions py-4 mt-lg-0 ">
               
-              <div ><ChatBox user={user} /> 
+              <div ><ChatBox user={user} />  </div>
              
               {props.isConnected &&  <Button
                
